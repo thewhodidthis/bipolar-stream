b52ba2c61d3e9a08ce58e1aeb564a9f92e2d780e
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-06-21 19:02:02 +0200

    example: fix for eslint

--- example/index.js
+++ example/index.js
@@ -1,8 +1,8 @@
 const Filter = require('../');
+
 const output = new Filter(100);
 
-console.log('Starting from 100,');
-console.log('seed with random numbers and log deltas:');
+console.log('Starting from 100, seed with random numbers and log deltas:');
 
 const feed = setInterval((stream) => {
   const input = Math.floor(Math.random() * 100) - 50;
@@ -13,11 +13,13 @@ const feed = setInterval((stream) => {
   stream.write(input.toString());
 }, 200, output);
 
-const stop = setTimeout(() => {
+const stop = () => {
   output.end();
   console.log('\n----\n');
   clearInterval(feed);
-}, 1000);
+};
+
+setTimeout(stop, 1000);
 
 output.pipe(process.stdout);
 output.on('error', console.error);
