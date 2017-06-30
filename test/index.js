5c6da15eaa0e8c9803699b7d35262b8b9c487de4
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-06-30 20:08:43 +0200

    test: meh, go back to using tape

--- test/index.js
+++ test/index.js
@@ -1,12 +1,19 @@
-const assert = require('assert');
+const test = require('tape');
 const Transform = require('stream').Transform;
 const Filter = require('../');
 
-assert.ok(Filter.prototype instanceof Transform);
+test('does inherit', (t) => {
+  t.ok(Filter.prototype instanceof Transform, 'is a transform stream');
+  t.end();
+});
 
-const input = 100;
-const bipolar = new Filter(input);
+test('will compute', (t) => {
+  const input = 100;
+  const bipolar = new Filter(input);
 
-bipolar.write(input.toString());
-assert.equal(parseInt(bipolar.read(), 10), input - input);
+  bipolar.write(input.toString());
+
+  t.equals(parseInt(bipolar.read(), 10), input - input);
+  t.end();
+});
 
