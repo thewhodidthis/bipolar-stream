9868d9070d361a1d4d23a169d4ea95e19ae420ab
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-06-30 18:42:43 +0200

    test: just use node assert

--- test/index.js
+++ test/index.js
@@ -1,19 +1,12 @@
-const test = require('tape');
+const assert = require('assert');
 const Transform = require('stream').Transform;
 const Filter = require('../');
 
-test('does inherit', (t) => {
-  t.ok(Filter.prototype instanceof Transform, 'is a transform stream');
-  t.end();
-});
+assert.ok(Filter.prototype instanceof Transform);
 
-test('will compute', (t) => {
-  const input = 100;
-  const bipolar = new Filter(input);
+const input = 100;
+const bipolar = new Filter(input);
 
-  bipolar.write(input.toString());
-
-  t.equals(parseInt(bipolar.read(), 10), input - input);
-  t.end();
-});
+bipolar.write(input.toString());
+assert.equal(parseInt(bipolar.read(), 10), input - input);
 
