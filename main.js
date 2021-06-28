9282addbc5bc5107987022a3228c632cefacc69d
Sotiri Bakagiannis <sotirios.bakagiannis@gmail.com>
2021-06-28 21:28:29 +0100

    module: fix for deno lint

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function bipolar(prev) {
   let memo = prev
 
   return new Transform({
-    transform(chunk, encoding, callback) {
+    transform(chunk, _, callback) {
       const next = parseFloat(chunk)
       const diff = next - memo
 
